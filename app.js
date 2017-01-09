c96c272b324ee4475553dd319bfcf240c1c4d309
Eve D <evedenison@gmail.com>
2017-01-09 14:48:48 -0800

    created store object literals

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
+
+var firstPike = {
+  minCust: 23,
+  maxCust: 65,
+  AvgCookSales: 6.3
+};
+
+var SeaTacAir = {
+  minCust: 3,
+  maxCust: 24,
+  AvgCookSales: 1.2
+};
+
+var SeaCenter = {
+  minCust: 11,
+  maxCust: 38,
+  AvgCookSales: 3.7
+};
+
+var CapHill = {
+  minCust: 20,
+  maxCust: 38,
+  AvgCookSales: 2.3
+};
+
+var Alki = {
+  minCust: 2,
+  maxCust: 16,
+  AvgCookSales: 4.6
+};
