147bf246b61a649bf3bc88067149f921408252ae
Brandon Little <brlittle86@gmail.com>
2017-01-11 17:08:24 -0800

    debugged name placement on table

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', 'Total: '];
-var stores = ['First and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+var storeHours = [,'6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM', 'Total: '];
+var stores = [];
 
 //Object Constructor
-function Store (minCust, maxCust, avgCookSales){
+function Store (storeName, minCust, maxCust, avgCookSales){
+  this.storeName = storeName;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookSales = avgCookSales;
@@ -39,8 +40,8 @@ Store.prototype.table = function() {
   this.totalCook();
   var tableEl = document.getElementById('stores');
   console.log(tableEl + 'tableEl');
-  for(var i = 0; i < stores.length; i++){
-    var rowData = stores[i];
+  for(var i = 0; i < 1; i++){
+    var rowData = this.storeName;
   //loop through inside of stores data
     var rowEl = document.createElement('tr');
     console.log('results stores for loop', this.results);
@@ -64,23 +65,26 @@ Store.prototype.table = function() {
 };
 
 //Instance of 1st and Pike Store
-var firstPike = new Store(23, 65, 6.3);
+var firstPike = new Store('First and Pike', 23, 65, 6.3);
 //Calling obeject methods and DOM code
 firstPike.table();
 
 //Instance of SeaTac Airport Store
-var seaTacAir = new Store(3, 24, 1.2);
+var seaTacAir = new Store('SeaTac Airport', 3, 24, 1.2);
 //Calling obeject methods and DOM code
 seaTacAir.table();
 //Instance of Seattle Center Store
-var seaCenter = new Store(11, 38, 3.7);
+var seaCenter = new Store('Seattle Center', 11, 38, 3.7);
 //Calling obeject methods and DOM code
 seaCenter.table();
 //Instance of Capitol Hill Store
-var capHill = new Store(20, 38, 2.3);
+var capHill = new Store('Capitol Hill', 20, 38, 2.3);
 //Calling obeject methods and DOM code
 capHill.table();
 //Instance of Alki Store
-var alki = new Store(2, 16, 4.6);
+var alki = new Store('Alki', 2, 16, 4.6);
 //Calling obeject methods and DOM code
 alki.table();
+
+//fill stores array with store objects
+stores.push(firstPike, seaTacAir, seaCenter, capHill, alki);
